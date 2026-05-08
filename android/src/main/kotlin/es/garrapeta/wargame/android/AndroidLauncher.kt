d264b5549a58e027a879afcf1321fa5889e80245
Sergio Torres <sergiotorresperez@gmail.com>
2026-05-08 22:34:50 +0100

    initial commit de gdx liftoff

---
+++ android/src/main/kotlin/es/garrapeta/wargame/android/AndroidLauncher.kt
@@ -0,0 +1,18 @@
+package es.garrapeta.wargame.android
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import es.garrapeta.wargame.Main
+
+/** Launches the Android application. */
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initialize(Main(), AndroidApplicationConfiguration().apply {
+            // Configure your application here.
+            useImmersiveMode = true // Recommended, but not required.
+        })
+    }
+}
